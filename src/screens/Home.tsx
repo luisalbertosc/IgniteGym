ebf43769292d0ef08ed65da11d4723bcd4d2aada
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-01-18 20:25:43 -0300

    Screens

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export const Home = () => {
+  return (
+    <Center>
+      <Text>Home</Text>
+    </Center>
+  );
+};
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export const Profile = () => {
+  return (
+    <Center>
+      <Text>Profile</Text>
+    </Center>
+  );
+};

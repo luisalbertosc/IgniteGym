f965b64f9db65e37e8f5694819041709cea4e4ee
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-01-17 21:08:46 -0300

    Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,13 @@
 import { VStack, Image, Text, Center, Heading } from "native-base";
 
+import { Input } from "@components/Input";
+
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
 export const SignIn = () => {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas praticando exercícios"
@@ -25,6 +27,13 @@ export const SignIn = () => {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
+
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );

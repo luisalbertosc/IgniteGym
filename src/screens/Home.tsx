4b0d7292a792605ae5e9d9246121df8decf31dfb
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-01-18 22:09:07 -0300

    Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,40 @@
-import { Text, VStack } from "native-base";
+import { useState } from "react";
 
+import { FlatList, HStack, VStack } from "native-base";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 
 export const Home = () => {
+  const [groupSelected, setGroupSelected] = useState("Costas");
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "ombro",
+  ]);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 };
